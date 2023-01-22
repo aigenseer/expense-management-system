c575ff6c68cc2ad61db78c66ed5f88e3e60fa949
aigenseer <viktor.aigenseer@web.de>
2023-01-22 15:43:06 +0100

    add bridge functions

--- 2-cleanproject-application/src/main/java/de/dhbw/cleanproject/application/book/UserApplicationService.java
+++ 2-cleanproject-application/src/main/java/de/dhbw/cleanproject/application/book/UserApplicationService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,12 +15,20 @@ public class UserApplicationService {
 
     private final UserRepository userRepository;
 
-    public List<User> findAllUsers() {
-        return userRepository.findAllUsers();
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 
-    public User saveUsers(User user) {
-        return userRepository.saveUser(user);
+    public Optional<User> findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
     }
 
 }
