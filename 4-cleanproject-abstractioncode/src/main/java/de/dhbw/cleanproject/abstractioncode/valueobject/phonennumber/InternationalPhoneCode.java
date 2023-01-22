cdf6a31958e33f913696908a30abb4a0af947cdd
aigenseer <viktor.aigenseer@web.de>
2023-01-22 13:49:37 +0100

    add getter functions

--- 4-cleanproject-abstractioncode/src/main/java/de/dhbw/cleanproject/abstractioncode/valueobject/phonennumber/InternationalPhoneCode.java
+++ 4-cleanproject-abstractioncode/src/main/java/de/dhbw/cleanproject/abstractioncode/valueobject/phonennumber/InternationalPhoneCode.java
@@ -1,11 +1,11 @@
 package de.dhbw.cleanproject.abstractioncode.valueobject.phonennumber;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public enum InternationalPhoneCode {
     DE("+49");
-
     private final String code;
-
 }
