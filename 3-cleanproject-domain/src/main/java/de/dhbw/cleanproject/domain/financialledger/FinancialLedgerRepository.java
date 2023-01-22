903a517f7cd87a38c8d524d4f02e4f323294c0e5
aigenseer <viktor.aigenseer@web.de>
2023-01-22 17:01:10 +0100

    create

---
+++ 3-cleanproject-domain/src/main/java/de/dhbw/cleanproject/domain/financialledger/FinancialLedgerRepository.java
@@ -0,0 +1,16 @@
+package de.dhbw.cleanproject.domain.financialledger;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface FinancialLedgerRepository {
+
+    List<FinancialLedger> findAll();
+
+    Optional<FinancialLedger> findById(UUID id);
+
+    FinancialLedger save(FinancialLedger user);
+
+    void deleteById(UUID id);
+}
