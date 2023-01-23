c2e5c20447ed2783242bfb97f1a677dea08c90be
aigenseer <viktor.aigenseer@web.de>
2023-01-23 17:30:13 +0100

    create controller of financial ledger user

---
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/rest/financialledger/user/FinancialLedgerUserController.java
@@ -0,0 +1,47 @@
+package de.dhbw.plugins.rest.financialledger.user;
+
+import de.dhbw.cleanproject.adapter.user.preview.UserPreview;
+import de.dhbw.cleanproject.adapter.user.preview.UserToUserPreviewModelMapper;
+import de.dhbw.cleanproject.application.book.UserApplicationService;
+import de.dhbw.cleanproject.domain.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping(value = "/api/financialledger/{financialLedgerId}/user/{financialLedgerUserId}", produces = "application/vnd.siren+json")
+@AllArgsConstructor
+
+public class FinancialLedgerUserController {
+
+    private final UserApplicationService userApplicationService;
+    private final UserToUserPreviewModelMapper userToUserPreviewModelMapper;
+
+    @GetMapping
+    public ResponseEntity<UserPreview> findOne(@PathVariable("financialLedgerId") UUID financialLedgerId, @PathVariable("financialLedgerUserId") UUID financialLedgerUserId) {
+        Optional<User> optionalUser = userApplicationService.findById(financialLedgerUserId);
+        if (!optionalUser.isPresent() || !userApplicationService.hasUserPermissionToFinancialLedger(financialLedgerUserId, financialLedgerId))
+            new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        User user = optionalUser.get();
+        UserPreview userPreview = userToUserPreviewModelMapper.apply(user);
+
+        Link selfLink = linkTo(methodOn(this.getClass()).findOne(financialLedgerId, financialLedgerUserId)).withSelfRel()
+                .andAffordance(afford(methodOn(this.getClass()).delete(financialLedgerId, financialLedgerUserId)));
+        userPreview.add(selfLink);
+
+        return ResponseEntity.ok(userPreview);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable("financialLedgerId") UUID financialLedgerId, @PathVariable("financialLedgerUserId") UUID financialLedgerUserId) {
+        return null;
+    }
+
+}
