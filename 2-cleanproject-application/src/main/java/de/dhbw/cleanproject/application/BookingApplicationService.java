16751d5a66efe852406b239dc7f6e4cf9dcc2bf1
aigenseer <viktor.aigenseer@web.de>
2023-01-23 20:16:09 +0100

    create booking application service

---
+++ 2-cleanproject-application/src/main/java/de/dhbw/cleanproject/application/BookingApplicationService.java
@@ -0,0 +1,34 @@
+package de.dhbw.cleanproject.application;
+
+import de.dhbw.cleanproject.domain.booking.Booking;
+import de.dhbw.cleanproject.domain.booking.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BookingApplicationService {
+
+    private final BookingRepository repository;
+
+    public List<Booking> findAllWithFinancialLedgerId(UUID financialLedgerId) {
+        return repository.findAllWithFinancialLedgerId(financialLedgerId);
+    }
+
+    public Optional<Booking> findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    public Booking save(Booking booking) {
+        return repository.save(booking);
+    }
+
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
+
+}
