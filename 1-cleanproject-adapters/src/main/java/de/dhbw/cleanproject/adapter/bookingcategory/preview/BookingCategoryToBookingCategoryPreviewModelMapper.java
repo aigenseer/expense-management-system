a387d88961556445eeee1137639fe188a5f5b78a
aigenseer <viktor.aigenseer@web.de>
2023-01-23 20:15:50 +0100

    create booking category preview model mapper

---
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/bookingcategory/preview/BookingCategoryToBookingCategoryPreviewModelMapper.java
@@ -0,0 +1,21 @@
+package de.dhbw.cleanproject.adapter.bookingcategory.preview;
+
+import de.dhbw.cleanproject.domain.bookingcategory.BookingCategory;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class BookingCategoryToBookingCategoryPreviewModelMapper implements Function<BookingCategory, BookingCategoryPreviewModel> {
+
+    @Override
+    public BookingCategoryPreviewModel apply(final BookingCategory category) {
+        return map(category);
+    }
+
+    private BookingCategoryPreviewModel map(final BookingCategory category) {
+        return BookingCategoryPreviewModel.builder()
+                .name(category.getTitle())
+                .build();
+    }
+}
