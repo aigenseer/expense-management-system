a2577dc61cda10484a478c574fcd2b2bcde2b316
aigenseer <viktor.aigenseer@web.de>
2023-01-23 20:13:17 +0100

    create booking repository bridge

---
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/persistence/hibernate/booking/BookingRepositoryBridge.java
@@ -0,0 +1,37 @@
+package de.dhbw.plugins.persistence.hibernate.booking;
+
+import de.dhbw.cleanproject.domain.booking.Booking;
+import de.dhbw.cleanproject.domain.booking.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Repository
+public class BookingRepositoryBridge implements BookingRepository {
+
+    private final SpringDataBookingRepository repository;
+
+    @Override
+    public List<Booking> findAllWithFinancialLedgerId(UUID financialLedgerId) {
+        return repository.findAllWithFinancialLedgerId(financialLedgerId);
+    }
+
+    @Override
+    public Optional<Booking> findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Booking save(Booking booking) {
+        return repository.save(booking);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
+}
