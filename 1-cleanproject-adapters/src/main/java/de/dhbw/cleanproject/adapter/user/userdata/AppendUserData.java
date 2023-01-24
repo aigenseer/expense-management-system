230c8423813a8c6462f2ef0030c144669bf3e514
aigenseer <viktor.aigenseer@web.de>
2023-01-24 21:07:16 +0100

    class redesigned as a general model

--- 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/user/userdata/AppendUserData.java
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/user/userdata/AppendUserData.java
@@ -1,10 +1,10 @@
-package de.dhbw.cleanproject.adapter.financialledger.data;
+package de.dhbw.cleanproject.adapter.user.userdata;
 
 import de.dhbw.cleanproject.adapter.config.customvalidatior.ValueOfUUID;
 import lombok.Data;
 
 @Data
-public class FinancialLedgerUserAppendData {
+public class AppendUserData {
 
     @ValueOfUUID(message = "The userId is invalid UUID.")
     private String userId;
