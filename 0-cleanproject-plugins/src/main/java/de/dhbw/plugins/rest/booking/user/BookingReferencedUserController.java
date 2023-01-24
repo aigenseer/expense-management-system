58663a2de8b7497343a3c71898eadc1d5a50f1e1
aigenseer <viktor.aigenseer@web.de>
2023-01-24 20:55:49 +0100

    create booking referenced user controller

---
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/rest/booking/user/BookingReferencedUserController.java
@@ -0,0 +1,53 @@
+package de.dhbw.plugins.rest.booking.user;
+
+import de.dhbw.cleanproject.adapter.user.preview.UserPreview;
+import de.dhbw.cleanproject.adapter.user.preview.UserToUserPreviewModelMapper;
+import de.dhbw.cleanproject.application.UserOperationService;
+import de.dhbw.cleanproject.domain.booking.Booking;
+import de.dhbw.cleanproject.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping(value = "/api/{userId}/financialledger/{financialLedgerId}/booking/{bookingId}/user/{referencedUserId}", produces = "application/vnd.siren+json")
+@RequiredArgsConstructor
+public class BookingReferencedUserController {
+
+    private final UserOperationService userOperationService;
+    private final UserToUserPreviewModelMapper userToUserPreviewModelMapper;
+
+
+    @GetMapping
+    public ResponseEntity<UserPreview> findOne(@PathVariable("userId") UUID userId, @PathVariable("financialLedgerId") UUID financialLedgerId, @PathVariable("bookingId") UUID bookingId, @PathVariable("referencedUserId") UUID referencedUserId) {
+        Optional<Booking> optionalBooking = userOperationService.getBooking(userId, financialLedgerId, bookingId);
+        if (!optionalBooking.isPresent()) new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        Booking booking = optionalBooking.get();
+        Optional<User> optionalReferencedUser = booking.getReferencedUsers().stream().filter(user -> user.getId().equals(referencedUserId)).findFirst();
+        if (!optionalReferencedUser.isPresent()) new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        User referencedUser = optionalReferencedUser.get();
+        UserPreview model = userToUserPreviewModelMapper.apply(referencedUser);
+
+        Link selfLink = linkTo(methodOn(getClass()).findOne(userId, financialLedgerId, bookingId, referencedUserId)).withSelfRel()
+                .andAffordance(afford(methodOn(getClass()).delete(userId, financialLedgerId, bookingId, referencedUserId)));
+        model.add(selfLink);
+
+        return ResponseEntity.ok(model);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<Void> delete(@PathVariable("userId") UUID userId, @PathVariable("financialLedgerId") UUID financialLedgerId, @PathVariable("bookingId") UUID bookingId, @PathVariable("referencedUserId") UUID referencedUserId) {
+
+        return null;
+    }
+
+
+
+}
