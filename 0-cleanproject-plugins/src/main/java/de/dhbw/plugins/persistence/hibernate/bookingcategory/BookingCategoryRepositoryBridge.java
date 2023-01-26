861b693178698e687ca740bbc760419f8184f821
aigenseer <viktor.aigenseer@web.de>
2023-01-26 20:58:29 +0100

    create booking category repository bridge

---
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/persistence/hibernate/bookingcategory/BookingCategoryRepositoryBridge.java
@@ -0,0 +1,31 @@
+package de.dhbw.plugins.persistence.hibernate.bookingcategory;
+
+import de.dhbw.cleanproject.domain.bookingcategory.BookingCategory;
+import de.dhbw.cleanproject.domain.bookingcategory.BookingCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Repository
+public class BookingCategoryRepositoryBridge implements BookingCategoryRepository {
+
+    private final SpringDataBookingCategoryRepository repository;
+
+    @Override
+    public Optional<BookingCategory> findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public BookingCategory save(BookingCategory bookingCategory) {
+        return repository.save(bookingCategory);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
+}
