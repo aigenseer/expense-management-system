5699a7b761bc14e4d0373c733c3533ef69e5fa18
aigenseer <viktor.aigenseer@web.de>
2023-01-30 18:54:18 +0100

    create startup test

---
+++ 0-cleanproject-plugins/src/test/java/de/dhbw/CleanProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package de.dhbw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@ComponentScan("de.dhbw")
+public class CleanProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
