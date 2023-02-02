f6c0f80dc74dd97c5e6461e959723c0da4e9a0ab
aigenseer <viktor.aigenseer@web.de>
2023-02-02 21:35:49 +0100

    create UserApplicationServiceTest test

---
+++ 0-cleanproject-plugins/src/test/java/de/dhbw/cleanproject/application/UserApplicationServiceTest.java
@@ -0,0 +1,92 @@
+package de.dhbw.cleanproject.application;
+
+import de.dhbw.cleanproject.abstractioncode.valueobject.email.Email;
+import de.dhbw.cleanproject.abstractioncode.valueobject.phonennumber.InternationalPhoneCode;
+import de.dhbw.cleanproject.abstractioncode.valueobject.phonennumber.PhoneNumber;
+import de.dhbw.cleanproject.domain.user.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest()
+@ComponentScan("de.dhbw")
+public class UserApplicationServiceTest {
+
+    @Autowired
+    private UserApplicationService applicationService;
+
+    private final User entity1 = User.builder()
+            .id(UUID.fromString("12345678-1234-1234-a123-123456789001"))
+            .email(new Email("example-user-1@example.de"))
+            .name("Example-User-1")
+            .phoneNumber(new PhoneNumber(123456789, InternationalPhoneCode.DE))
+        .build();
+
+    private final User entity2 = User.builder()
+            .id(UUID.fromString("12345678-1234-1234-a123-123456789003"))
+            .email(new Email("example-user-3@example.de"))
+            .name("Example-User-3")
+            .phoneNumber(new PhoneNumber(123456789, InternationalPhoneCode.DE))
+            .build();
+
+    @Test
+    public void testSave() {
+        applicationService.save(entity2);
+        Optional<User> result = applicationService.findById(entity2.getId());
+        assertTrue(result.isPresent());
+        checkEntity(entity2, result.get());
+    }
+
+    @Test
+    public void testDeleteById() {
+        applicationService.deleteById(entity1.getId());
+        Optional<User> result = applicationService.findById(entity1.getId());
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void testFindAll(){
+        List<User> resultList = applicationService.findAll();
+        assertEquals(2, resultList.size());
+        Optional<User> result = resultList.stream().filter(user -> user.getId().equals( entity1.getId())).findFirst();
+        assertTrue(result.isPresent());
+        checkEntity(entity1, result.get());
+    }
+
+    @Test
+    public void testFindById(){
+        Optional<User> result = applicationService.findById(entity1.getId());
+        assertTrue(result.isPresent());
+        checkEntity(entity1, result.get());
+    }
+
+    @Test
+    public void testFindAllById() {
+        List<User> resultList = applicationService.findAllById(new ArrayList<UUID>(){{ add(entity1.getId()); }});
+        assertEquals(1, resultList.size());
+        Optional<User> result = resultList.stream().filter(user -> user.getId().equals( entity1.getId())).findFirst();
+        assertTrue(result.isPresent());
+        checkEntity(entity1, result.get());
+    }
+
+    private void checkEntity(User expectedEntity, User actualEntity){
+        assertEquals(expectedEntity.getId(), actualEntity.getId());
+        assertEquals(expectedEntity.getEmail(), actualEntity.getEmail());
+        assertEquals(expectedEntity.getName(), actualEntity.getName());
+        assertEquals(expectedEntity.getPhoneNumber(), actualEntity.getPhoneNumber());
+
+    }
+
+}
