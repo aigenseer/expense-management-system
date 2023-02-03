92fd048ae02c299b076bd1a20b7aa41271985f41
aigenseer <viktor.aigenseer@web.de>
2023-02-03 21:41:55 +0100

    add setter

--- 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/financialledger/model/FinancialLedgerModel.java
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/financialledger/model/FinancialLedgerModel.java
@@ -1,13 +1,19 @@
 package de.dhbw.cleanproject.adapter.financialledger.model;
 
+import de.dhbw.cleanproject.adapter.booking.preview.BookingPreviewCollectionModel;
+import de.dhbw.cleanproject.adapter.bookingcategory.preview.BookingCategoryPreviewCollectionModel;
 import de.dhbw.cleanproject.adapter.user.preview.UserPreviewCollectionModel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
 @Builder
 @Getter
+@Setter
 public class FinancialLedgerModel extends RepresentationModel<FinancialLedgerModel>{
     private String name;
     private UserPreviewCollectionModel userPreviewCollectionModel;
+    private BookingCategoryPreviewCollectionModel bookingCategoryPreviewCollectionModel;
+    private BookingPreviewCollectionModel bookingPreviewCollectionModel;
 }
