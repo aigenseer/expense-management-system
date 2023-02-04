472e4ed1c16b78beddfcfe2d5dd712351ff47226
aigenseer <viktor.aigenseer@web.de>
2023-02-04 13:06:51 +0100

    Add id, setter function to collection attribute

--- 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/user/usermodel/UserModel.java
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/user/usermodel/UserModel.java
@@ -1,15 +1,22 @@
 package de.dhbw.cleanproject.adapter.user.usermodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import de.dhbw.cleanproject.adapter.financialledger.preview.FinancialLedgerPreviewCollectionModel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
+
+import java.util.UUID;
 
 @Builder
 @Getter
 public class UserModel extends RepresentationModel<UserModel>{
+    @JsonIgnore
+    private UUID id;
     private String name;
     private String email;
     private String phoneNumber;
-    private FinancialLedgerPreviewCollectionModel financialLedgers;
+    @Setter
+    private FinancialLedgerPreviewCollectionModel financialLedgerPreviewCollectionModel;
 }
