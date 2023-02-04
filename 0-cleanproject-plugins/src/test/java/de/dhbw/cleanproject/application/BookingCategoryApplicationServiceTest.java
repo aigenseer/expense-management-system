2ee2ee0fa0efcb4bb8de704d70cad343f6a5e4b3
aigenseer <viktor.aigenseer@web.de>
2023-02-04 19:57:55 +0100

    add testCreateByAttributeData, testUpdateByAttributeData

--- 0-cleanproject-plugins/src/test/java/de/dhbw/cleanproject/application/BookingCategoryApplicationServiceTest.java
+++ 0-cleanproject-plugins/src/test/java/de/dhbw/cleanproject/application/BookingCategoryApplicationServiceTest.java
@@ -1,6 +1,7 @@
 package de.dhbw.cleanproject.application;
 
 import de.dhbw.cleanproject.application.bookingcategory.BookingCategoryApplicationService;
+import de.dhbw.cleanproject.application.bookingcategory.BookingCategoryAttributeData;
 import de.dhbw.cleanproject.domain.bookingcategory.BookingCategory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,11 +27,15 @@ public class BookingCategoryApplicationServiceTest {
     private final BookingCategory entity1 = BookingCategory.builder()
             .id(UUID.fromString("12345678-1234-1234-a123-123456789021"))
             .title("Example-Category-1")
-        .build();
+            .build();
 
     private final BookingCategory entity2 = BookingCategory.builder()
             .id(UUID.fromString("12345678-1234-1234-a123-123456789022"))
             .title("Example-Category-2")
+            .build();
+
+    private final BookingCategoryAttributeData attributeData = BookingCategoryAttributeData.builder()
+            .title("NewTitle")
             .build();
 
     @Test
@@ -49,15 +54,37 @@ public class BookingCategoryApplicationServiceTest {
     }
 
     @Test
-    public void testFindById(){
+    public void testFindById() {
         Optional<BookingCategory> result = applicationService.findById(entity1.getId());
         assertTrue(result.isPresent());
         checkEntity(entity1, result.get());
     }
 
-    private void checkEntity(BookingCategory expectedEntity, BookingCategory actualEntity){
+    private void checkEntity(BookingCategory expectedEntity, BookingCategory actualEntity) {
         assertEquals(expectedEntity.getId(), actualEntity.getId());
         assertEquals(expectedEntity.getTitle(), actualEntity.getTitle());
+    }
+
+    @Test
+    public void testCreateByAttributeData() {
+        Optional<BookingCategory> optionalBookingCategory = applicationService.createByAttributeData(entity1.getFinancialLedger(), attributeData);
+        assertTrue(optionalBookingCategory.isPresent());
+        checkAttributeData(attributeData, optionalBookingCategory.get());
+        assertEquals(entity1.getFinancialLedger(), optionalBookingCategory.get().getFinancialLedger());
+    }
+
+    @Test
+    public void testUpdateByAttributeData() {
+        Optional<BookingCategory> optionalBookingCategory = applicationService.updateByAttributeData(entity1, attributeData);
+        assertTrue(optionalBookingCategory.isPresent());
+        checkAttributeData(attributeData, optionalBookingCategory.get());
+        optionalBookingCategory = applicationService.findById(entity1.getId());
+        assertTrue(optionalBookingCategory.isPresent());
+        checkAttributeData(attributeData, optionalBookingCategory.get());
+    }
+
+    private void checkAttributeData(BookingCategoryAttributeData attributeData, BookingCategory actualEntity) {
+        assertEquals(attributeData.getTitle(), actualEntity.getTitle());
     }
 
 }
