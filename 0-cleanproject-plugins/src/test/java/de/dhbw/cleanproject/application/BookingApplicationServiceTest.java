7b413b16fa46871c007a78bbd9a058e6d46d5483
aigenseer <viktor.aigenseer@web.de>
2023-02-04 20:18:47 +0100

    add testCreateByAttributeData, testUpdateByAttributeData

--- 0-cleanproject-plugins/src/test/java/de/dhbw/cleanproject/application/BookingApplicationServiceTest.java
+++ 0-cleanproject-plugins/src/test/java/de/dhbw/cleanproject/application/BookingApplicationServiceTest.java
@@ -1,7 +1,15 @@
 package de.dhbw.cleanproject.application;
 
+import de.dhbw.cleanproject.abstractioncode.valueobject.money.CurrencyType;
+import de.dhbw.cleanproject.abstractioncode.valueobject.money.Money;
 import de.dhbw.cleanproject.application.booking.BookingApplicationService;
+import de.dhbw.cleanproject.application.booking.BookingAttributeData;
+import de.dhbw.cleanproject.application.bookingcategory.BookingCategoryAttributeData;
+import de.dhbw.cleanproject.application.financialledger.FinancialLedgerApplicationService;
 import de.dhbw.cleanproject.domain.booking.Booking;
+import de.dhbw.cleanproject.domain.bookingcategory.BookingCategory;
+import de.dhbw.cleanproject.domain.financialledger.FinancialLedger;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +21,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
@@ -25,12 +34,19 @@ public class BookingApplicationServiceTest {
 
     private final Booking entity1 = Booking.builder()
             .id(UUID.fromString("12345678-1234-1234-a123-123456789031"))
+            .financialLedger(FinancialLedger.builder().build())
             .title("Example-Booking")
-        .build();
+            .build();
 
     private final Booking entity2 = Booking.builder()
             .id(UUID.fromString("12345678-1234-1234-a123-123456789032"))
             .title("Example-Booking-2")
+            .build();
+
+    private final BookingAttributeData attributeData = BookingAttributeData.builder()
+            .title("NewTitle")
+            .money(new Money(19.99, CurrencyType.EURO))
+            .bookingCategory(null)
             .build();
 
     @Test
@@ -58,6 +74,30 @@ public class BookingApplicationServiceTest {
     private void checkEntity(Booking expectedEntity, Booking actualEntity){
         assertEquals(expectedEntity.getId(), actualEntity.getId());
         assertEquals(expectedEntity.getTitle(), actualEntity.getTitle());
+    }
+
+    @Test
+    public void testCreateByAttributeData() {
+        Optional<Booking> optionalBooking = applicationService.createByAttributeData(entity1.getUser(), entity1.getFinancialLedger(), attributeData);
+        assertTrue(optionalBooking.isPresent());
+        assertEquals(entity1.getUser(), optionalBooking.get().getUser());
+        assertEquals(entity1.getFinancialLedger().getId(), optionalBooking.get().getFinancialLedgerId());
+    }
+
+    @Test
+    public void testUpdateByAttributeData() {
+        Optional<Booking> optionalBooking = applicationService.updateByAttributeData(entity1, attributeData);
+        assertTrue(optionalBooking.isPresent());
+        checkAttributeData(attributeData, optionalBooking.get());
+        optionalBooking = applicationService.findById(entity1.getId());
+        assertTrue(optionalBooking.isPresent());
+        checkAttributeData(attributeData, optionalBooking.get());
+    }
+
+    private void checkAttributeData(BookingAttributeData attributeData, Booking actualEntity) {
+        assertEquals(attributeData.getTitle(), actualEntity.getTitle());
+        assertEquals(attributeData.getMoney(), actualEntity.getMoney());
+        assertEquals(attributeData.getBookingCategory(), actualEntity.getCategory());
     }
 
 }
