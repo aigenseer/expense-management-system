3948961898d8537a6edae03373687262aa0c27fc
aigenseer <viktor.aigenseer@web.de>
2023-02-04 17:33:21 +0100

    create adapter mapping

--- 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/booking/data/BookingUnsafeDataToBookingAttributeDataAdapterMapper.java
+++ 1-cleanproject-adapters/src/main/java/de/dhbw/cleanproject/adapter/booking/data/BookingUnsafeDataToBookingAttributeDataAdapterMapper.java
@@ -2,41 +2,31 @@ package de.dhbw.cleanproject.adapter.booking.data;
 
 import de.dhbw.cleanproject.abstractioncode.valueobject.money.CurrencyType;
 import de.dhbw.cleanproject.abstractioncode.valueobject.money.Money;
+import de.dhbw.cleanproject.application.booking.BookingAttributeData;
 import de.dhbw.cleanproject.application.bookingcategory.BookingCategoryApplicationService;
-import de.dhbw.cleanproject.domain.booking.Booking;
 import de.dhbw.cleanproject.domain.bookingcategory.BookingCategory;
-import de.dhbw.cleanproject.domain.user.User;
 import lombok.RequiredArgsConstructor;
-import org.javatuples.Triplet;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
-public class BookingDataToBookingMapper implements Function<Triplet<IBookingData, UUID, User>, Booking> {
+public class BookingUnsafeDataToBookingAttributeDataAdapterMapper implements Function<IBookingUnsafeData, BookingAttributeData> {
 
     private final BookingCategoryApplicationService bookingCategoryApplicationService;
 
     @Override
-    public Booking apply(final Triplet<IBookingData, UUID, User> triplet) {
-        return map(triplet);
+    public BookingAttributeData apply(final IBookingUnsafeData data) {
+        return map(data);
     }
 
-    private Booking map(final Triplet<IBookingData, UUID, User> triplet) {
-        IBookingData data = triplet.getValue0();
-        UUID financialLedgerId = triplet.getValue1();
-        User user = triplet.getValue2();
-        Booking.BookingBuilder builder = Booking.builder();
-        builder.id(UUID.randomUUID());
-        builder.title(data.getTitle());
-        builder.financialLedgerId(financialLedgerId);
-        builder.user(user);
-        builder.creationDate(LocalDate.now());
-        if (data.getTitle() != null) builder.title(data.getTitle());
+    private BookingAttributeData map(final IBookingUnsafeData data) {
+        BookingAttributeData.BookingAttributeDataBuilder builder = BookingAttributeData.builder();
+        if (data.getTitle() != null)
+            builder.title(data.getTitle());
         try {
             CurrencyType currencyType = CurrencyType.valueOf(data.getCurrencyType());
             if(data.getCurrencyType() != null){
@@ -47,9 +37,8 @@ public class BookingDataToBookingMapper implements Function<Triplet<IBookingData
         if(data.getBookingCategoryId() != null){
             UUID bookingCategoryId = UUID.fromString(data.getBookingCategoryId());
             Optional<BookingCategory> optionalBooking = bookingCategoryApplicationService.findById(bookingCategoryId);
-            optionalBooking.ifPresent(builder::category);
+            optionalBooking.ifPresent(builder::bookingCategory);
         }
-
         return builder.build();
     }
 
