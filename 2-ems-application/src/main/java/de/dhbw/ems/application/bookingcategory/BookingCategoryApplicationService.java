b991a17ec54defb3d79d812f3abe5c083c5c7843
aigenseer <viktor.aigenseer@web.de>
2023-02-11 18:34:11 +0100

    rename interface

--- 2-ems-application/src/main/java/de/dhbw/ems/application/bookingcategory/BookingCategoryApplicationService.java
+++ 2-ems-application/src/main/java/de/dhbw/ems/application/bookingcategory/BookingCategoryApplicationService.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class BookingCategoryApplicationService implements BookingCategoryDomainService {
+public class BookingCategoryApplicationService implements BookingCategoryDomainServicePort {
 
     private final BookingCategoryRepository repository;
 
--- 2-ems-application/src/main/java/de/dhbw/ems/application/bookingcategory/BookingCategoryDomainServicePort.java
+++ 2-ems-application/src/main/java/de/dhbw/ems/application/bookingcategory/BookingCategoryDomainServicePort.java
@@ -6,7 +6,7 @@ import de.dhbw.ems.domain.financialledger.FinancialLedger;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface BookingCategoryDomainService {
+public interface BookingCategoryDomainServicePort {
 
     Optional<BookingCategory> findById(UUID id);
 
