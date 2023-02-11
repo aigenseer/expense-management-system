ea8a0fdc4de418ef50c23fa3cbfada5635dc5e48
aigenseer <viktor.aigenseer@web.de>
2023-02-11 19:27:42 +0100

    create adapter interface

--- 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/financialledger/preview/FinancialLedgerToFinancialLedgerPreviewModelAdapterMapper.java
+++ 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/financialledger/preview/FinancialLedgerToFinancialLedgerPreviewModelAdapterMapper.java
@@ -1,21 +1,8 @@
 package de.dhbw.ems.adapter.model.financialledger.preview;
 
 import de.dhbw.ems.domain.financialledger.FinancialLedger;
-import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
-@Component
-public class FinancialLedgerToFinancialLedgerPreviewModelAdapterMapper implements Function<FinancialLedger, FinancialLedgerPreviewModel> {
-
-    @Override
-    public FinancialLedgerPreviewModel apply(final FinancialLedger financialLedger) {
-        return map(financialLedger);
-    }
-
-    private FinancialLedgerPreviewModel map(final FinancialLedger financialLedger) {
-        return FinancialLedgerPreviewModel.builder()
-                .name(financialLedger.getTitle())
-                .build();
-    }
+public interface FinancialLedgerToFinancialLedgerPreviewModelAdapterMapper extends Function<FinancialLedger, FinancialLedgerPreviewModel> {
 }
