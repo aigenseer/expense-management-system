c942d90d9182779264b0ac811492dbd4264fb2c8
aigenseer <viktor.aigenseer@web.de>
2023-02-11 19:16:33 +0100

    create adapter interface

---
+++ 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/booking/data/BookingUnsafeDataToAttributeDataMapper.java
@@ -0,0 +1,44 @@
+package de.dhbw.ems.adapter.model.booking.data;
+
+import de.dhbw.ems.abstractioncode.valueobject.money.CurrencyType;
+import de.dhbw.ems.abstractioncode.valueobject.money.Money;
+import de.dhbw.ems.application.booking.BookingAttributeData;
+import de.dhbw.ems.application.bookingcategory.BookingCategoryDomainServicePort;
+import de.dhbw.ems.domain.bookingcategory.BookingCategory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class BookingUnsafeDataToAttributeDataMapper implements BookingUnsafeDataToBookingAttributeDataAdapterMapper {
+
+    private final BookingCategoryDomainServicePort bookingCategoryDomainServicePort;
+
+    @Override
+    public BookingAttributeData apply(final IBookingUnsafeData data) {
+        return map(data);
+    }
+
+    private BookingAttributeData map(final IBookingUnsafeData data) {
+        BookingAttributeData.BookingAttributeDataBuilder builder = BookingAttributeData.builder();
+        if (data.getTitle() != null)
+            builder.title(data.getTitle());
+        try {
+            CurrencyType currencyType = CurrencyType.valueOf(data.getCurrencyType());
+            if(data.getCurrencyType() != null){
+                Money money = new Money(data.getAmount(), currencyType);
+                builder.money(money);
+            }
+        }catch (IllegalArgumentException|NullPointerException ignored){}
+        if(data.getBookingCategoryId() != null){
+            UUID bookingCategoryId = UUID.fromString(data.getBookingCategoryId());
+            Optional<BookingCategory> optionalBooking = bookingCategoryDomainServicePort.findById(bookingCategoryId);
+            optionalBooking.ifPresent(builder::bookingCategory);
+        }
+        return builder.build();
+    }
+
+}
