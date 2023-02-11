c781e7585f94abd348b0f7e0ec4e50017cfae454
aigenseer <viktor.aigenseer@web.de>
2023-02-11 19:19:46 +0100

    create adapter interface

---
+++ 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/bookingcategory/data/BookingCategoryDataToAttributeDataMapper.java
@@ -0,0 +1,19 @@
+package de.dhbw.ems.adapter.model.bookingcategory.data;
+
+import de.dhbw.ems.application.bookingcategory.BookingCategoryAttributeData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookingCategoryDataToAttributeDataMapper implements BookingCategoryDataToBookingCategoryAttributeDataAdapterMapper {
+
+    @Override
+    public BookingCategoryAttributeData apply(final BookingCategoryData data) {
+        return map(data);
+    }
+
+    private BookingCategoryAttributeData map(final BookingCategoryData data) {
+        return BookingCategoryAttributeData.builder().title(data.getTitle()).build();
+    }
+}
