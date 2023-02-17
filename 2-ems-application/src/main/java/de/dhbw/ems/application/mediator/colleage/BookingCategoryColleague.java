275c83e6ff69ebf0e027ba747a94a7c0f525f067
aigenseer <viktor.aigenseer@web.de>
2023-02-17 20:35:58 +0100

    add the @Override annotation

--- 2-ems-application/src/main/java/de/dhbw/ems/application/mediator/colleage/BookingCategoryColleague.java
+++ 2-ems-application/src/main/java/de/dhbw/ems/application/mediator/colleage/BookingCategoryColleague.java
@@ -14,6 +14,7 @@ public class BookingCategoryColleague extends Colleague {
         this.bookingCategoryDomainService = bookingCategoryDomainService;
     }
 
+    @Override
     public void onDeleteBookingCategory(BookingCategory bookingCategory) {
         bookingCategoryDomainService.deleteById(bookingCategory.getId());
     }
