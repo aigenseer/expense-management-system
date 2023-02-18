d680be56f5de0ed2391e68ebcb47af302794da4c
aigenseer <viktor.aigenseer@web.de>
2023-02-18 19:35:04 +0100

    remove private constructor

--- 0-ems-plugins/src/main/java/de/dhbw/plugins/config/HateoasConfiguration.java
+++ 0-ems-plugins/src/main/java/de/dhbw/plugins/config/HateoasConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HateoasConfiguration {
 
-    private HateoasConfiguration(){}
-
     @Bean
     public SirenConfiguration sirenConfiguration() {
         return new SirenConfiguration().withEntityAndCollectionModelSubclassingEnabled(true);
