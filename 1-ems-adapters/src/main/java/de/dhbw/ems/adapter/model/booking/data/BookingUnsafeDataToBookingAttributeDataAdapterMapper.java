2b55180286e08f031675ce80f404aa91ad9b944c
aigenseer <viktor.aigenseer@web.de>
2023-02-23 18:46:50 +0100

    replace BookingAttributeData with BookingAggregateAttributeData

--- 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/booking/data/BookingUnsafeDataToBookingAttributeDataAdapterMapper.java
+++ 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/model/booking/data/BookingUnsafeDataToBookingAttributeDataAdapterMapper.java
@@ -1,8 +1,8 @@
 package de.dhbw.ems.adapter.model.booking.data;
 
-import de.dhbw.ems.application.booking.BookingAttributeData;
+import de.dhbw.ems.application.booking.data.BookingAggregateAttributeData;
 
 import java.util.function.Function;
 
-public interface BookingUnsafeDataToBookingAttributeDataAdapterMapper extends Function<IBookingUnsafeData, BookingAttributeData> {
+public interface BookingUnsafeDataToBookingAttributeDataAdapterMapper extends Function<IBookingUnsafeData, BookingAggregateAttributeData> {
 }
