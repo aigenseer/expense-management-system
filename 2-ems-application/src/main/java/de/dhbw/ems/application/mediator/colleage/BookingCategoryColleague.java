328f82eca61ef0f64b21bc11ff3efa2ca4ee93c9
aigenseer <viktor.aigenseer@web.de>
2023-02-24 20:58:53 +0100

    remove aggregate with entity

--- 2-ems-application/src/main/java/de/dhbw/ems/application/mediator/colleage/BookingCategoryColleague.java
+++ 2-ems-application/src/main/java/de/dhbw/ems/application/mediator/colleage/BookingCategoryColleague.java
@@ -1,22 +1,26 @@
 package de.dhbw.ems.application.mediator.colleage;
 
-import de.dhbw.ems.application.bookingcategory.BookingCategoryDomainService;
+import de.dhbw.ems.application.bookingcategory.aggregate.BookingCategoryAggregateDomainService;
+import de.dhbw.ems.application.bookingcategory.entity.BookingCategoryDomainService;
 import de.dhbw.ems.application.mediator.ConcreteApplicationMediator;
-import de.dhbw.ems.domain.bookingcategory.entity.BookingCategory;
+import de.dhbw.ems.domain.bookingcategory.aggregate.BookingCategoryAggregate;
 
 
 public class BookingCategoryColleague extends Colleague {
 
+    private final BookingCategoryAggregateDomainService bookingCategoryAggregateDomainService;
     private final BookingCategoryDomainService bookingCategoryDomainService;
 
-    public BookingCategoryColleague(final ConcreteApplicationMediator mediator, final BookingCategoryDomainService bookingCategoryDomainService) {
+    public BookingCategoryColleague(final ConcreteApplicationMediator mediator, final BookingCategoryAggregateDomainService bookingCategoryAggregateDomainService, final BookingCategoryDomainService bookingCategoryDomainService) {
         super(mediator);
+        this.bookingCategoryAggregateDomainService = bookingCategoryAggregateDomainService;
         this.bookingCategoryDomainService = bookingCategoryDomainService;
     }
 
     @Override
-    public void onDeleteBookingCategory(BookingCategory bookingCategory) {
-        bookingCategoryDomainService.deleteById(bookingCategory.getId());
+    public void onDeleteBookingCategory(BookingCategoryAggregate bookingCategoryAggregate) {
+        bookingCategoryAggregateDomainService.deleteById(bookingCategoryAggregate.getId());
+        bookingCategoryDomainService.deleteById(bookingCategoryAggregate.getBookingCategoryId());
     }
 
 }
