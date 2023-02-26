f62926667922987defcfdcdaa63a266be9ef6c58
aigenseer <viktor.aigenseer@web.de>
2023-02-26 13:19:30 +0100

    add findByCreatorId Methode

--- 0-ems-plugins/src/main/java/de/dhbw/plugins/persistence/hibernate/booking/aggregate/SpringDataBookingAggregateRepository.java
+++ 0-ems-plugins/src/main/java/de/dhbw/plugins/persistence/hibernate/booking/aggregate/SpringDataBookingAggregateRepository.java
@@ -2,9 +2,14 @@ package de.dhbw.plugins.persistence.hibernate.booking.aggregate;
 
 import de.dhbw.ems.domain.booking.aggregate.BookingAggregate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SpringDataBookingAggregateRepository extends JpaRepository<BookingAggregate, UUID> {
+
+    @Query("SELECT b FROM BookingAggregate b WHERE b.creatorId = ?1")
+    List<BookingAggregate> findByCreatorId(UUID creatorId);
 
 }
