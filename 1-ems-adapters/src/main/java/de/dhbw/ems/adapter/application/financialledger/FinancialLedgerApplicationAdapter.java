0d4230df02de5eabdffb6174973680347532d8d8
aigenseer <viktor.aigenseer@web.de>
2023-02-26 13:18:42 +0100

    replace FinancialLedger entity with aggregate

--- 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/application/financialledger/FinancialLedgerApplicationAdapter.java
+++ 1-ems-adapters/src/main/java/de/dhbw/ems/adapter/application/financialledger/FinancialLedgerApplicationAdapter.java
@@ -1,17 +1,19 @@
 package de.dhbw.ems.adapter.application.financialledger;
 
 import de.dhbw.ems.application.archive.core.TmpFile;
-import de.dhbw.ems.application.financialledger.FinancialLedgerAttributeData;
-import de.dhbw.ems.domain.financialledger.FinancialLedger;
+import de.dhbw.ems.application.financialledger.data.FinancialLedgerAttributeData;
+import de.dhbw.ems.domain.financialledger.aggregate.FinancialLedgerAggregate;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FinancialLedgerApplicationAdapter {
 
-    Optional<FinancialLedger> create(UUID userId, FinancialLedgerAttributeData attributeData);
 
-    Optional<FinancialLedger> find(UUID id, UUID financialLedgerId);
+    Optional<FinancialLedgerAggregate> create(UUID userId, FinancialLedgerAttributeData attributeData);
+
+    Optional<FinancialLedgerAggregate> find(UUID id, UUID financialLedgerId);
 
     boolean unlinkUser(UUID id, UUID financialLedgerId);
 
@@ -21,8 +23,10 @@ public interface FinancialLedgerApplicationAdapter {
 
     boolean delete(UUID id, UUID financialLedgerId);
 
-    Optional<FinancialLedger> updateByAttributeData(FinancialLedger financialLedger, FinancialLedgerAttributeData data);
+    Optional<FinancialLedgerAggregate> updateByAttributeData(FinancialLedgerAggregate financialLedgerAggregate, FinancialLedgerAttributeData data);
 
-    TmpFile createTmpZipArchive(FinancialLedger financialLedger);
+    TmpFile createTmpZipArchive(FinancialLedgerAggregate financialLedgerAggregate);
+
+    List<FinancialLedgerAggregate> findFinancialLedgerAggregatesByUserId(UUID userId);
 
 }
