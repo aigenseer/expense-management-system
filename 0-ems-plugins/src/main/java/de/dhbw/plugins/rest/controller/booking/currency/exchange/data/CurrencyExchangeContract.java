efb9257681f09b8f3c3eb6aff74c942d21b79982
aigenseer <viktor.aigenseer@web.de>
2023-03-01 17:35:12 +0100

    moved

--- 0-ems-plugins/src/main/java/de/dhbw/plugins/rest/controller/booking/currency/exchange/data/CurrencyExchangeContract.java
+++ 0-ems-plugins/src/main/java/de/dhbw/plugins/rest/controller/booking/currency/exchange/data/CurrencyExchangeContract.java
@@ -1,14 +1,15 @@
-package de.dhbw.ems.adapter.api.currency.exchange;
+package de.dhbw.plugins.rest.controller.booking.currency.exchange.data;
 
 import de.dhbw.ems.abstractioncode.valueobject.money.CurrencyType;
-import de.dhbw.ems.adapter.model.customvalidatior.ValueOfEnum;
+import de.dhbw.ems.adapter.api.currency.exchange.ICurrencyExchangeContract;
+import de.dhbw.plugins.rest.customvalidatior.ValueOfEnum;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 
 
 @Data
-public class CurrencyExchangeContract{
+public class CurrencyExchangeContract implements ICurrencyExchangeContract {
 
     @NotEmpty(message = "The target currencyType is required.")
     @ValueOfEnum(enumClass = CurrencyType.class, message = "Target invalid currency type")
