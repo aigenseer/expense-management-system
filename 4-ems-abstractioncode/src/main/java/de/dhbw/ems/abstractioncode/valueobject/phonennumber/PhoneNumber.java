84f3b4cbff1e4b6910a341f958aed21012678e7e
aigenseer <viktor.aigenseer@web.de>
2023-03-02 09:44:28 +0100

    add validation

--- 4-ems-abstractioncode/src/main/java/de/dhbw/ems/abstractioncode/valueobject/phonennumber/PhoneNumber.java
+++ 4-ems-abstractioncode/src/main/java/de/dhbw/ems/abstractioncode/valueobject/phonennumber/PhoneNumber.java
@@ -1,39 +1,51 @@
 package de.dhbw.ems.abstractioncode.valueobject.phonennumber;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.util.Objects;
 
 @Embeddable
-@RequiredArgsConstructor
-@AllArgsConstructor
+@Getter
 public class PhoneNumber  {
 
-    @Getter
-    private Integer phoneNumber;
-    @Getter
+    private Integer number;
     private InternationalPhoneCode internationalPhoneCode;
+
+    public PhoneNumber(){}
+
+    public PhoneNumber(
+            final Integer number,
+            final InternationalPhoneCode internationalPhoneCode
+    ){
+        validatePhoneNumber(number);
+        this.number = number;
+        this.internationalPhoneCode = internationalPhoneCode;
+    }
+
+    private void validatePhoneNumber(final Integer number){
+        if (number <= 1){
+            throw new RuntimeException("Number cannot be negative.");
+        }
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PhoneNumber that = (PhoneNumber) o;
-        return Double.compare(that.phoneNumber, phoneNumber) == 0 && internationalPhoneCode == that.internationalPhoneCode;
+        return Double.compare(that.number, number) == 0 && internationalPhoneCode == that.internationalPhoneCode;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(phoneNumber, internationalPhoneCode);
+        return Objects.hash(number, internationalPhoneCode);
     }
 
     @Override
     public String toString(){
-        return String.format("%s %s", internationalPhoneCode.getCode(), phoneNumber);
+        return String.format("%s %s", internationalPhoneCode.getCode(), number);
     }
 
 }
