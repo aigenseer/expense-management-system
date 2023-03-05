cad76a5ff55a511f9b03d042e9861b8a45e8133a
aigenseer <viktor.aigenseer@web.de>
2023-03-05 17:43:35 +0100

    implement interface and rename attribute

--- 0-ems-plugins/src/main/java/de/dhbw/plugins/rest/controller/financialledgers/data/FinancialLedgerData.java
+++ 0-ems-plugins/src/main/java/de/dhbw/plugins/rest/controller/financialledgers/data/FinancialLedgerData.java
@@ -1,15 +1,16 @@
-package de.dhbw.ems.adapter.mapper.data.financialledger;
+package de.dhbw.plugins.rest.controller.financialledgers.data;
 
+import de.dhbw.ems.adapter.mapper.data.financialledger.IFinancialLedgerData;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
-public class FinancialLedgerData {
+public class FinancialLedgerData implements IFinancialLedgerData {
 
     @NotEmpty(message = "The name is required.")
     @Size(min = 2, max = 100, message = "The length of name must be between 2 and 100 characters.")
-    private String name;
+    private String title;
 
 }
